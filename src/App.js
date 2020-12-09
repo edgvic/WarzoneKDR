5e28d40ee90abb6b8b536d64d81221c108185e77
edgvic <56657911+edgvic@users.noreply.github.com>
2020-12-09 02:25:32 +0100

    front end

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import Particles, { InteractivityDetect } from 'react-particles-js';
+import Navigation from './components/Navigation/Navigation.js';
+import Logo from './components/Logo/Logo.js';
+import TagForm from './components/TagForm/TagForm.js';
 import './App.css';
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 100,
+      density: {
+        enable: true,
+        value_area: 500
+      }
+    }
+  },
+  interactivity: {
+    onhover: {
+      enable: true
+    }
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Particles className="particles"
+              params={particlesOptions}
+            />
+      <Navigation />
+      <Logo />
+      <TagForm />
+      {/*<Stats />*/}
     </div>
   );
 }
