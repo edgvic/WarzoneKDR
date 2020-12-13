0d89f262183a61b9ccb5ad33a4c419566441fd36
edgvic <56657911+edgvic@users.noreply.github.com>
2020-12-13 02:42:09 +0100

    sign in - log in- register - rank count

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import UrlForm from './components/UrlForm/UrlForm.js';
 import SignIn from './components/SignIn/SignIn.js';
 import Register from './components/Register/Register.js';
 import BackgroundRemoval from './components/BackgroundRemoval/BackgroundRemoval.js';
+import Rank from './components/Rank/Rank.js';
 
 import './App.css';
 
@@ -27,18 +28,36 @@ const particlesOptions = {
 }
 
 class App extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       input: '',
       imageUrl: '',
       fetched: false,
       route: 'signin',
-      isSignedIn: false
+      isSignedIn: false,
+      user: {
+        id: '',
+        name: 'asd',
+        email: '',
+        entries: 0,
+        joined: ''
+      }
     }
     this.onButtonSubmit = this.onButtonSubmit.bind(this);
   }
 
+
+  loadUser = (user) => {
+    this.setState({user: {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        entries: user.entries,
+        joined: user.joined
+        }
+    })
+  }
 
   onInputChange = (event) => {
     this.setState({input: event.target.value});
@@ -52,22 +71,32 @@ class App extends Component {
     var unirest = require("unirest");
 
     var req = unirest("POST", "https://background-removal.p.rapidapi.com/remove");
-    
+
     req.headers({
       "content-type": "application/x-www-form-urlencoded",
-      "x-rapidapi-key": "c7688b5c0cmsh4fbf294031282f9p11c9b9jsndcc4c5c219da",
+      "x-rapidapi-key": "14be53dd46mshb96e7293ae4d8cep10628ajsn33aca4aa3080",
       "x-rapidapi-host": "background-removal.p.rapidapi.com",
       "useQueryString": true
     });
-    
+
     req.form({
       "image_url": this.state.input
     });
-    
+
     
     req.end(function (res) {
       if (res.error) throw new Error(res.error);
-
+      fetch('http://localhost:3000/image', {
+        method: 'put',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+        id: currentComponent.state.user.id
+        })
+      })
+      .then(response => response.json())
+      .then(count => {
+        currentComponent.setState(Object.assign(currentComponent.state.user, {entries: count}))
+      })
       currentComponent.setState({imageUrl: res.body.response.image_url});
       currentComponent.setState({fetched: true});
     });
@@ -95,6 +124,7 @@ class App extends Component {
         {this.state.route === 'home' 
         ?<div>
           <Logo />
+          <Rank user={this.state.user}/>
           <UrlForm 
             onInputChange={this.onInputChange}
             onButtonSubmit={this.onButtonSubmit}
@@ -103,8 +133,8 @@ class App extends Component {
         </div>
         : (
             this.state.route === 'signin'
-            ? <SignIn onRouteChange={this.onRouteChange}/>
-            : <Register onRouteChange={this.onRouteChange}/>
+            ? <SignIn loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+            : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
           )
         }
       </div>
